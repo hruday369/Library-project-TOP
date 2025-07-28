1176ff9f9dfc4e5130a8b4584289698e524e96a3
Hruday369 <thirumalahruday2000@gmail.com>
2025-07-28 23:07:26 +0530

    "form completed"

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
-const myLibrary = [];
+// const myLibrary = [];
 
-function Book(name,title,pages) {
-  this.name=name;
-  this.title=title;
-  this.pages=pages;
-}
+// function Book(name,title,pages) {
+//   this.name=name;
+//   this.title=title;
+//   this.pages=pages;
+// }
 
-function addBookToLibrary() {
-  // take params, create a book then store it in the array
-  const book = new Book('C','james',245);
-  myLibrary.push(book);
-}
+// function addBookToLibrary() {
+//   // take params, create a book then store it in the array
+//   const book = new Book('C','james',245);
+//   myLibrary.push(book);
+// }
+const formdialog = document.querySelector("#formDialog");
+const addbook = document.querySelector("#addBook");
+const closeform = document.querySelector("#close");
+
+addbook.addEventListener('click',()=>{
+    formdialog.showModal();
+});
+closeform.addEventListener('click',()=>{
+    formddialog.close();
+});
