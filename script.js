4f042f759bcfb38ecf8b43d2dfe208080b212f81
Hruday369 <thirumalahruday2000@gmail.com>
2025-08-02 09:00:05 +0530

    display content

--- script.js
+++ script.js
@@ -1,23 +1,74 @@
-// const myLibrary = [];
+const myLibrary = [];
 
-// function Book(name,title,pages) {
-//   this.name=name;
-//   this.title=title;
-//   this.pages=pages;
-// }
+function Book(title, author, pages, read) {
+  this.id = crypto.randomUUID(); // Unique ID for each book
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+}
+// Function to add a new book to the library
+function addBookToLibrary(title, author, pages, read) {
+  const newBook = new Book(title, author, pages, read);
+  myLibrary.push(newBook);
+  displayBooks(); // Refresh the display after adding a new book
+}
+function displayBooks(){
+  let displaybook = document.querySelector("#displaybook");
+  displaybook.innerHTML='';
+  myLibrary.forEach((book) =>{
+    const cardhtml = `
+              <div>
+            <h1>${book.title}</h1>
+            <p>${book.author}</p>
+            <p>${book.pages}</p>
+            <button id="readbtn">${book.read}</button>
+             </div>`;
+    const tempdiv = document.createElement('div');
+    tempdiv.innerHTML = cardhtml;
+    const bookcard = tempdiv.firstElementChild;
+    displaybook.appendChild(bookcard);
+  });
 
-// function addBookToLibrary() {
-//   // take params, create a book then store it in the array
-//   const book = new Book('C','james',245);
-//   myLibrary.push(book);
-// }
-const formdialog = document.querySelector("#formDialog");
+}
+const dialog = document.querySelector("dialog");
 const addbook = document.querySelector("#addBook");
-const closeform = document.querySelector("#close");
+const addlibrary = document.querySelector("#addtoLibrary");
+const closedialog = document.querySelector("#close");
 
-addbook.addEventListener('click',()=>{
-    formdialog.showModal();
+
+addbook.addEventListener('click',() =>{
+  dialog.showModal();
 });
-closeform.addEventListener('click',()=>{
-    formddialog.close();
+
+closedialog.addEventListener('click',() =>{
+  dialog.close();
+});
+
+addlibrary.addEventListener('click',(event) =>{
+  event.preventDefault();
+
+  const booktitle = document.querySelector("#title").value;
+  const authorname = document.querySelector("#name").value;
+  const noofpages = document.querySelector("#pages").value;
+  const bookread = document.querySelector("#read").checked;
+  
+  // empty field inputs.
+  if(authorname === ''){
+    alert('please enter the anthor name.');
+    return;
+  }
+  if(booktitle === ''){
+    alert('please enter title of the book.');
+    return;
+  }
+  if(noofpages === '' || parseInt(noofpages) <= 0 || isNaN(noofpages)){
+    alert('please enter No of pages (Greater than 0).');
+    return;
+  }
+  
+  addBookToLibrary(booktitle,authorname,noofpages,bookread);
+  dialog.close();
+
+
 });
